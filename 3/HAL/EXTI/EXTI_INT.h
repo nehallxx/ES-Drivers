138f20383b6b08ccc097e0cfac142d6604925919
nehallxx <132005828+nehallxx@users.noreply.github.com>
2023-08-18 23:31:14 +0300

    Delete 3 directory

--- 3/HAL/EXTI/EXTI_INT.h
+++
@@ -1,6 +0,0 @@
-#ifndef HAL_EXTI_EXTI_INT_H_
-#define HAL_EXTI_EXTI_INT_H_
-void EXTI_voidInit(void);
-
-
-#endif /* HAL_EXTI_EXTI_INT_H_ */
--- 3/HAL/GIE/GIE_INT.h
+++
@@ -1,7 +0,0 @@
-#ifndef HAL_GIE_GIE_INT_H_
-#define HAL_GIE_GIE_INT_H_
-
-void GIE_voidEnableGlobalInterrupt(void);
-void GIE_voidDisableGlobalInterrupt(void);
-
-#endif /* HAL_GIE_GIE_INT_H_ */
